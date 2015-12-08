ba755fa8900a1c6ac077c2c1b8fda5c5c801bed6
wdhorton <wdt.horton@gmail.com>
2015-12-08 14:21:58 -0500

    Install and configure Guard and Rspec.

--- Gemfile
+++ Gemfile
@@ -5,3 +5,4 @@ gem "rspec", "~> 3.1.0"
 gem "activesupport"
 gem 'sqlite3'
 gem 'bcrypt'
+gem 'guard-rspec', require: false
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,11 @@
+require_relative '../../app/models/user'
+
+describe User do
+  describe ".find_by_credentials" do
+    it "returns nil when given incorrect password"
+
+    it "returns nil when given incorrect username"
+
+    it "returns the correct user for given credentials"
+  end
+end
